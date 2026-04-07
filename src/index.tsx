9bf72c14e3950d8fc3a49bfc8d60055163073449
Ryan Lane <ryanlane@mappedsky.com>
2026-04-07 20:15:30 +0900

    Add CSP nonce support for MUI styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+function getCspNonce(): string | undefined {
+  const nonceMeta = document.querySelector('meta[property="csp-nonce"]');
+  return nonceMeta?.getAttribute('content') ?? undefined;
+}
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
+const emotionCache = createCache({
+  key: 'mui',
+  nonce: getCspNonce()
+});
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <CacheProvider value={emotionCache}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CacheProvider>
 );
